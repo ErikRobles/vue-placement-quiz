8daf123383602b3c300207f4d7619697ec849cbc
ErikRobles <erikjames69@hotmail.com>
2020-02-27 15:01:10 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <HeaderSection />
+    <QuestionSection />
+  </div>
+</template>
+<script>
+import HeaderSection from "/components/HeaderSection";
+import QuestionSection from "/components/QuestionSection";
+export default {
+  name: "App",
+  components: {
+    HeaderSection,
+    QuestionSection
+  }
+};
+</script>
---
+++ src/components/HeaderSection.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <img class="logo" src="/img/lion.png" alt />
+    <h1 id="heading-top">Lionsfield English Quick Placement Quiz</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "HeaderSection"
+};
+</script>
